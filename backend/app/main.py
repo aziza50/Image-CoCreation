3b67a74a31bfc018064570c7ea1208c4bb422b8d
aziza50 <aziza.ergasheva50@gmail.com>
2026-03-24 00:12:00 -0400

    Debug vector database and feature extractions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import get_settings
-from app.api.routes.onboarding import router as onboarding_router
+from app.api.routes.extract_features import router as extract_features_router
 
 app = FastAPI(title = "Image Co-Creation",
               version = "0.1.0",
@@ -13,13 +13,13 @@ settings = get_settings()
 #Add to middleware to allow requests from frontend from my .env file
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = settings.allowed_origins,
+    allow_origins = ["http://localhost:3000"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
 
-app.include_router(onboarding_router, prefix = "/onboarding", tags = ["onboarding"])
+app.include_router(extract_features_router, prefix = "/extract-features", tags = ["extract-features"])
 
 @app.get("/health")
 def health_check():
